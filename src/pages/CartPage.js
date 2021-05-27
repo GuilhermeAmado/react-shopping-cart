687d06f36212b17a30459b56f94c3c6af4ec3281
Guilherme Amado <guiii@live.com>
2021-05-26 21:42:03 -0300

    add button to checkout page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { CartContext } from '../CartContext';
+import CartButton from '../components/CartButton';
 import CartItem from '../components/CartItem';
 import CartItemsList from '../components/CartItemsList';
 import EmptyCartWarning from '../components/EmptyCartWarning';
@@ -16,6 +17,9 @@ const CartPage = () => {
           <CartItem key={item.id} item={item} />
         ))}
         {cartItems.length === 0 && <EmptyCartWarning />}
+        <CheckoutButtonContainer>
+          {cartItems.length > 0 && <CartButton>Finalizar compra</CartButton>}
+        </CheckoutButtonContainer>
       </CartItemsList>
     </div>
   );
@@ -24,6 +28,13 @@ const CartPage = () => {
 const PageTitle = styled.h1`
   text-align: center;
   margin-bottom: 40px;
+`;
+
+const CheckoutButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 export default CartPage;
